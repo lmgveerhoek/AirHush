7d46dfe7caa7ebfa5aad6f85200d51488af85435
Max Veerhoek <lmg@veerhoek.eu>
2024-05-13 20:58:19 +0200

    Initial Commit

---
+++ AirHush/AirHushApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirHushApp.swift
+//  AirHush
+//
+//  Created by Max Veerhoek on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirHushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirHush/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirHush
+//
+//  Created by Max Veerhoek on 13/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
